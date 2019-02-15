23c34aebe6abb081f7005dd5ea71c20204c918bb
Stephen Kaplan <stephen.kaplan@firstsolar.com>
2019-02-15 14:56:30 -0800

    added setup.py in preparation for registering plantpredict with Python Package Index (PyPI)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='plantpredict',
+    version='0.6.0',
+    description='Python 2.7 SDK for PlantPredict (https://ui.plantpredict.com).',
+    url='http://github.com/storborg/funniest',
+    author='Stephen Kaplan, Performance & Prediction Engineer at First Solar, Inc.',
+    author_email='',
+    license=None,
+    packages=['plantpredict'],
+    zip_safe=False
+)
