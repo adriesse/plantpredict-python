8a35887e18b850db7271a1cf3dae40296e094afe
Stephen Kaplan <stephen.kaplan@firstsolar.com>
2019-03-01 10:26:07 -0800

    import issue fix

--- plantpredict/oauth2.py
+++ plantpredict/oauth2.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import plantpredict
+from plantpredict import settings
 
 
 class OAuth2(object):
@@ -15,17 +15,17 @@ class OAuth2(object):
             params={
                 "grant_type": "password",
                 "scope": "openid offline_access",
-                "username": plantpredict.settings.USERNAME,
-                "password": plantpredict.settings.PASSWORD,
-                "client_id": plantpredict.settings.CLIENT_ID,
-                "client_secret": plantpredict.settings.CLIENT_SECRET
+                "username": settings.USERNAME,
+                "password": settings.PASSWORD,
+                "client_id": settings.CLIENT_ID,
+                "client_secret": settings.CLIENT_SECRET
             }
         )
 
         # set authentication token as global variable
         try:
-            plantpredict.settings.TOKEN = json.loads(response.content)['access_token']
-            plantpredict.settings.REFRESH_TOKEN = json.loads(response.content)['refresh_token']
+            settings.TOKEN = json.loads(response.content)['access_token']
+            settings.REFRESH_TOKEN = json.loads(response.content)['refresh_token']
         except KeyError:
             pass
 
@@ -37,22 +37,19 @@ class OAuth2(object):
             url="https://afse.okta.com/oauth2/aus3jzhulkrINTdnc356/v1/token",
             headers={"content-type": "application/x-www-form-urlencoded"},
             data={
-                "refresh_token": plantpredict.settings.REFRESH_TOKEN,
+                "refresh_token": settings.REFRESH_TOKEN,
                 "grant_type": "refresh_token",
                 "scope": "offline_access",
-                "client_id": plantpredict.settings.CLIENT_ID,
-                "client_secret": plantpredict.settings.CLIENT_SECRET
+                "client_id": settings.CLIENT_ID,
+                "client_secret": settings.CLIENT_SECRET
             }
         )
 
         # set authentication token as global variable
         try:
-            plantpredict.settings.TOKEN = json.loads(response.content)['access_token']
-            plantpredict.settings.REFRESH_TOKEN = json.loads(response.content)['refresh_token']
+            settings.TOKEN = json.loads(response.content)['access_token']
+            settings.REFRESH_TOKEN = json.loads(response.content)['refresh_token']
         except KeyError:
             pass
 
         return response
-
-    def __init__(self):
-        pass
